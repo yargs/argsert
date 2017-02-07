a2b5a348dcec3fccc62e9265f4ad0eb644a6276c
Jason Kurian <jasonk92@gmail.com>
2017-02-07 10:00:33 -0500

    Initial commit with opinions

---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+module.exports = function argsert (typeConfig, ...args) {
+  if (typeof typeConfig !== 'string' || (!isRequired(typeConfig) && !isOptional(typeConfig))) {
+    throw new Error('A type configuration string for your arguments is required.');
+  }
+
+  const types = getTypes(typeConfig);
+  const configuredKeys = Object.keys(types);
+  const numRequired = configuredKeys.filter(k => 'required' in types[k]).length;
+  args = args[args.length - 1] === undefined ? args.filter(v => v !== undefined) : args;
+
+  if (args.length < numRequired) {
+    throw new Error(`Not enough arguments provided. Expected ${numRequired} but received ${args.length}.`);
+  }
+
+  if (args.length > configuredKeys.length) {
+    throw new Error(`Too many arguments provided. Expected max ${configuredKeys.length} but received ${args.length}.`);
+  }
+
+  args.forEach((arg, index) => {
+    const searchElement = Array.isArray(arg) ? 'array' : arg === null ? 'null' : typeof arg;
+
+    if ('required' in types[index]) {
+      const required = types[index].required;
+
+      if (required.indexOf(searchElement) < 0 && required.indexOf('*') < 0) {
+        throw new Error(
+          `Invalid first argument. Expected ${expectedTypes(types[index], 'required')} but received ${searchElement}.`
+        );
+      }
+    } else if ('optional' in types[index]) {
+      if (types[index].optional.indexOf(searchElement) < 0) {
+        throw new Error(
+          `Invalid first argument. Expected ${expectedTypes(types[index], 'optional')} but received ${searchElement}.`
+        );
+      }
+    }
+  });
+
+  return true;
+};
+
+function getTypes (typeConfig) {
+  return typeConfig.split(' ').reduce((result, str, index) => {
+    if (isOptional(str)) {
+      return Object.assign({}, result, {
+        [index]: {
+          optional: str.split('|').map(s => s.replace('[', '').replace(']', ''))
+        }
+      });
+    } else if (isRequired(str)) {
+      return Object.assign({}, result, {
+        [index]: {
+          required: str.split('|').map(s => s.replace('<', '').replace('>', ''))
+        }
+      });
+    } else {
+      throw new Error(`invalid type config at pos: ${index}`);
+    }
+  }, {});
+}
+
+function expectedTypes (types, kind) {
+  return types[kind].join(' or ');
+}
+
+function isOptional (arg) {
+  return arg.match(/^\[(\w+)((?:\|(\w+))*?)]/);
+}
+
+function isRequired (arg) {
+  return arg.match(/^<(\w+|\*)((?:\|(\w+))*?)>/);
+}
---
+++ test.js
@@ -0,0 +1,68 @@
+import test from 'ava';
+import argsert from './';
+
+test('does not throw exception if optional argument is not provided', t => {
+  t.notThrows(() => argsert('[object]', ...arguments));
+});
+
+test('throws exception if wrong type is provided for optional argument', t => {
+  t.throws(
+    () => argsert('[object|number]', 'hello'),
+    /Invalid first argument. Expected object or number but received string./
+  );
+});
+
+test('does not throw exception if optional argument is valid', t => {
+  t.notThrows(() => argsert('[object]', {foo: 'bar'}));
+});
+
+test('throws exception if required argument is not provided', t => {
+  t.throws(
+    () => argsert('<object>'),
+    /Not enough arguments provided. Expected 1 but received 0./
+  );
+});
+
+test('throws exception if required argument is of wrong type', t => {
+  t.throws(
+    () => argsert('<object>', 'bar'),
+    /Invalid first argument. Expected object but received string./
+  );
+});
+
+test('supports a combination of required and optional arguments', t => {
+  t.notThrows(() => argsert('<array> <string|object> [string|object]', [], 'foo', {}));
+});
+
+test('throws an exception if too many arguments are provided', t => {
+  t.throws(
+    () => argsert('<array> [batman]', [], 33, 99),
+    /Too many arguments provided. Expected max 2 but received 3./
+  );
+});
+
+test.skip('configures function to accept 0 parameters, if only arguments object is provided', t => {
+  t.throws(
+    (bar = 99) => argsert(...arguments),
+    /A type configuration string for your arguments is required./
+  );
+});
+
+test('allows for any type if * is provided', t => {
+  t.notThrows(() => argsert('<*>', 'bar'));
+});
+
+test('should ignore trailing undefined values', t => {
+  t.notThrows(() => argsert('<*>', 'bar', undefined, undefined));
+});
+
+test('should not ignore undefined values that are not trailing', t => {
+  t.throws(
+    () => argsert('<*>', 'bar', undefined, undefined, 33),
+    /Too many arguments provided. Expected max 1 but received 4./
+  );
+});
+
+test('supports null as special type', t => {
+  t.notThrows(() => argsert('<null>', null));
+});

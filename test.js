dbbf4ca30f692771b506f42ad51c288cfea76b23
Jason Kurian <jasonk92@gmail.com>
2017-02-07 10:05:48 -0500

    tests: use true assertion on return value
    rather than notThrows

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import argsert from './';
 
 test('does not throw exception if optional argument is not provided', t => {
-  t.notThrows(() => argsert('[object]', ...arguments));
+  t.true(argsert('[object]'));
 });
 
 test('throws exception if wrong type is provided for optional argument', t => {
@@ -13,7 +13,7 @@ test('throws exception if wrong type is provided for optional argument', t => {
 });
 
 test('does not throw exception if optional argument is valid', t => {
-  t.notThrows(() => argsert('[object]', {foo: 'bar'}));
+  t.true(argsert('[object]', {foo: 'bar'}));
 });
 
 test('throws exception if required argument is not provided', t => {
@@ -31,7 +31,7 @@ test('throws exception if required argument is of wrong type', t => {
 });
 
 test('supports a combination of required and optional arguments', t => {
-  t.notThrows(() => argsert('<array> <string|object> [string|object]', [], 'foo', {}));
+  t.true(argsert('<array> <string|object> [string|object]', [], 'foo', {}));
 });
 
 test('throws an exception if too many arguments are provided', t => {
@@ -49,11 +49,11 @@ test.skip('configures function to accept 0 parameters, if only arguments object 
 });
 
 test('allows for any type if * is provided', t => {
-  t.notThrows(() => argsert('<*>', 'bar'));
+  t.true(argsert('<*>', 'bar'));
 });
 
 test('should ignore trailing undefined values', t => {
-  t.notThrows(() => argsert('<*>', 'bar', undefined, undefined));
+  t.true(argsert('<*>', 'bar', undefined, undefined));
 });
 
 test('should not ignore undefined values that are not trailing', t => {
@@ -64,5 +64,5 @@ test('should not ignore undefined values that are not trailing', t => {
 });
 
 test('supports null as special type', t => {
-  t.notThrows(() => argsert('<null>', null));
+  t.true(argsert('<null>', null));
 });
